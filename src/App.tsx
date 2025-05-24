5a56ce11b06a2527b637b2510f559f3bbbaca13c
gyoung314 <graceyoungccc@gmail.com>
2025-05-24 01:05:23 -0400

    CHRIST

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import { useEffect, useState } from "react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 import React from "react";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function App() {
 
